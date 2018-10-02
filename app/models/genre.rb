343c1c9d3c3e89fb5237680112a2cf1b3d338f4c
Adam Barton <adam.barton333@gmail.com>
2018-10-02 21:39:21 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,14 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
-    genre.songs.count
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artist.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.artist.name
   end
 end
