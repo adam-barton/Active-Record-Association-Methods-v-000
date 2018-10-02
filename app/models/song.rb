b70a4d61de5e5b1e4170d58b036725d5aefc93ec
Adam Barton <adam.barton333@gmail.com>
2018-10-02 21:52:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,6 +10,7 @@ class Song < ActiveRecord::Base
     # when this method is called it should assign the song's artist to Drake
     Artists.all.each do |artist|
       artist.name == Drake
-    artist.songs << self
+      artist.songs << self
+    end
   end
 end
